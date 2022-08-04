9d879e9440601e70f27046411795d8254f3a82c6
Chay-May <chayenne.mayara@gmail.com>
2022-08-03 22:48:55 -0300

    Exercício off 012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,23 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 45
+let isMaiorIdade = true
+let isCursandoOutraFaculdade = false
 
-if(){
 
-}else{
-	
+if(idade >= 18) {
+    alert('A pessoa tem 18 anos ou mais')
+} else {
+    alert ('A pessoa é menor de 18 anos')
 }
+
+if (isMaiorIdade === true) {
+    alert ("Já tem idade de ter terminado o ensino médio")
+} else {
+    alert ("Ainda não terminou o ensino médio")
+}
+
+if (isCursandoOutraFaculdade === false) {
+    alert('A pessoa não está na faculdade')
+} else {
+    alert ('A pessoa está na faculdade')
+}
+
